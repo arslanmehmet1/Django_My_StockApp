73fb0cd72b7fc6939ecc22ff9c0ae97b430d09be
Mehmet Arslan <marslan.developer@gmail.com>
2023-02-07 20:02:07 +0300

    little try

--- stock/signals.py
+++ stock/signals.py
@@ -1,10 +1,17 @@
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from .models import Purchases, Sales, Product
 
 
-# @receiver(post_save, sender=Purchases)
-# def create_Purchases(sender, instance=None, created=False, **kwargs):
-#     if created:
-#         Product.objects.create(user=instance)
+@receiver(pre_save, sender=Purchases)
+def create_Token(sender, instance, **kwargs):
+    product=Product.objects.get(id=instance.product_id)
+    product["stock"]+=instance.quantity
+    product.save()
+
+
+# Product.objects.create(user=instance)
+# @receiver(pre_save, sender=Sales)
+# def calculate_total_price(sender, instance, **kwargs):
+#     instance.price_total = instance.quantity * instance.price
